3a035cf5797e0b2be67cad66e3c740bd36bb41ce
yulongtan <swifties@uw.edu>
2016-01-30 17:26:53 -0800

    added vertical and horizontal loops for grabbing word selection

--- src/WordSearch.java
+++ src/WordSearch.java
@@ -12,8 +12,9 @@ public class WordSearch {
     public static final int DEFAULT = 11;
     public static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
 
-    private int[][] puzzle;
+    private char[][] puzzle;
     private SortedSet<String> wordList;
+    private String selection;
 
     public WordSearch() {
         this(DEFAULT, null);
@@ -30,7 +31,7 @@ public class WordSearch {
         if (words.size() < 1) {
             throw new IllegalArgumentException("List cannot be empty");
         }
-        this.puzzle = new int[n][n];
+        this.puzzle = new char[n][n];
         this.wordList = new TreeSet<String>();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -47,14 +48,36 @@ public class WordSearch {
     // should choose a selection of letters and group them together (word)
     // then check to see if that word is in the list of words
     // selects them from a starting coordinate and an ending one
-    public void select(int x1, int y1, int x2, int y2) {
-        String selection = "";
-        // 0,0 -> 0, 5
-        if (x1 == x2) {
 
-        } else { //diagonals?
+    /** STILL NEEDS THIS IMPLEMENTATION
+         * must account for negatives when doing this. going 'up'
+         * the puzzle is decreasing x
+         * going 'left' is decreasing y
+     */
+    public void select(int x1, int y1, int x2, int y2) {
+        this.selection = "";
+        // 0,0 -> 0,5, horizontal
+        if (x1 == x2) {
+            for (int i = y1; i < y2; i++) {
+                this.selection += this.puzzle[x1][i];
+            }
+        } else if (y1 == y2) {  // 7,3 to 9,3, vertical
+            for (int i = x1; i < x2; i++) {
+                this.selection += this.puzzle[i][y1];
+            }
+        } else { // diagonal
 
         }
+    }
+
+    // This method should set up the game by putting the list of words
+    // into the puzzle
+    public void setUp() {
+
+    }
+
+    public String getSelection() {
+        return this.selection;
     }
 
     // Word is considered valid even if found 'backwards'
